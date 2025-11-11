e6ea47fbe040f8a32cc909f49befb0d60803df70
Thomas Back <thomas.johan.back@gmail.com>
2025-11-10 16:52:13 -0800

    init

---
+++ src/main/java/com/example/wedding_story_api/WeddingStoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wedding_story_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingStoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingStoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wedding_story_api/WeddingStoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wedding_story_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingStoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
